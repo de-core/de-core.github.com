3de5b3fdca320516af61c22de5828d4d167c3e7c
Eugen Soloviov <suenot@gmail.com>
2016-04-11 22:43:42 +0300

    data.json

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@ var app = angular.module('decoreApp', []);
 
 	app.controller('generalController', ['$scope', '$http',
 		function($scope, $http){
-			$http.get('http://de-core.net/assets/data/data.json').success(function(data){
+			$http.get('/data/data.json').success(function(data){
 				$scope.projects = data;
 			}).error(function(data){
 				alert("Data json Error");
